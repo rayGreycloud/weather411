262c962bf6b1b046a80c5b439981b262b8b657d3
rayGreycloud <rayGreycloud@gmail.com>
2018-01-19 06:45:53 -0800

    Add change location event with modal

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const ui = new UI();
 // Call getWeather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change location event 
+document.getElementById('w-change-btn')
+  .addEventListener('click', (e) => {
+    // Get input values
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    // Change location
+    weather.changeLocation(city, state);
+    // Fetch and display weather 
+    getWeather();  
+    // Close modal 
+    $('#locModal').modal('hide');
+  });
+
 // Get weather function
 function getWeather() {
   weather.fetchWeather()
